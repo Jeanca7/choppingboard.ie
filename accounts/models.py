a919e2e89eeed0191c6a099941035c10cff1f247
Jean <jsepulvedadominguez@gmail.com>
2018-12-18 13:55:55 +0000

    default avatar

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    image = models.ImageField(upload_to="avatars", default="avatars/anonymous.png", null=False, blank=True)
+    image = models.ImageField(upload_to="avatars", default="avatars/anonymous.png", null=False, blank=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     date_of_birth = models.DateField(blank=True, null=True)
     
