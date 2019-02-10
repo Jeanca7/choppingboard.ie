a2864573c0e1d0e0fc4e64d7161638eb92f83857
Jean <jsepulvedadominguez@gmail.com>
2019-02-10 13:54:24 +0000

    setting social_auth

--- choppingboard/settings/base.py
+++ choppingboard/settings/base.py
@@ -73,7 +73,7 @@ AUTHENTICATION_BACKENDS = [
     'social_core.backends.facebook.FacebookOAuth2',
     'social_core.backends.twitter.TwitterOAuth',
     'social_core.backends.google.GoogleOAuth2',
-    'social.backends.linkedin.LinkedinOAuth2',
+    'social_core.backends.linkedin.LinkedinOAuth2',
     
 ]
 
